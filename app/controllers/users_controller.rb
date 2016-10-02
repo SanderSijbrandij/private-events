d1bd2173709cca7c3edfdc17d904237a582e59be
Sander Sijbrandij <ssijbrandij@gmail.com>
2016-10-02 14:56:56 +0200

    Add simple login/logout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       flash[:success] = "User created"
-      redirect_to user_path(@user)
+      redirect_to @user
     else
       render 'new'
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ApplicationRecord
+  before_save :downcase_email
   has_secure_password
+
+  private
+
+  def downcase_email
+    email.downcase!
+  end
 end
